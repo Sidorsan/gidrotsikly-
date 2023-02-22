48984602ec50b8aa676db792dfe01db83b3b5c5c
Sidor007 <Sidor007@yandex.ru>
2023-02-22 14:49:21 +0700

    search

--- pages/index.js
+++ pages/index.js
@@ -17,5 +17,24 @@ $(function () {
     $('.sliderBanner__slider').slick('slickPrev');
   })
 
-  
 })
+
+$(function () {
+    $('input[placeholder], textarea[placeholder]').blur();
+    $('.serMemdd').change(function () {
+        var k = $(this).val();
+      if (k == '1') {
+          console.log('1');
+            $("#serMemtb").attr("placeholder", "Type a name (Lastname, Firstname)").blur();
+        }
+        else if (k == '2') {
+            $("#serMemtb").attr("placeholder", "Type an ID").blur();
+        }
+        else if (k == '3') {
+            $("#serMemtb").attr("placeholder", "Type a Location").blur();
+        }
+    });
+});
+$(".search__form-top").on("click", function () {
+     $(".search__input").attr("placeholder", (("введите " + $( "input:checked" ).val()))).blur();
+  });
