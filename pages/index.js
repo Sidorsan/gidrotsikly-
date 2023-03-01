466d01b99b8095ea38cb599ffb98855f690daf43
Sidor007 <Sidor007@yandex.ru>
2023-03-01 11:58:58 +0700

    finishWithoutAdaptation

--- pages/index.js
+++ pages/index.js
@@ -86,56 +86,12 @@ $(function () {
   })
 })
 
-// var filtered = false;
-// $('.popular__filter-box li button').on('click',function(){
-//     $('.popular__filter-box li button').removeClass('active');
-//   var filter = $(this).data('filter');
-//   console.log(filter);
-//     if(filter=='all'){
-//         $('.popular__cardList').slick('slickUnfilter');
-//     }else if(filter == '.popular__card--spareParts') {
-//         $('.popular__cardList').slick('slickUnfilter').slick('slickFilter', filter);
-//     } else if (filter == '.popular__card--motors') {
-//        $('.popular__cardList').slick('slickUnfilter').slick('slickFilter', filter);
-//     }
-//     $(this).addClass('active');
-//     filtered = true;
-// });
 
-
-
-  // $(".popular__filter-box li button").on('click', function () {
-  //   var filter = $(this).data('filter');
-  //   console.log(filter);
-  //   $(".popular__cardList").slick('slickUnfilter');
-
-  //   if (filter == '.popular__card--spareParts') {
-  //     console.log(1);
-  //     $(".popular__cardList").slick('slickUnfilter').slick('slickFilter', '.popular__card--spareParts');
-  //   }
-  //   else if (filter == '.popular__card--motors') {
-  //           console.log(2);
-  //     $(".popular__cardList").slick('slickUnfilter').slick('slickFilter', '.popular__card--motors');
-  //   }
-  //   else if (filter == '.popular__card--tyres') {
-  //     console.log(3);
-  //     $(".popular__cardList").slick('slickUnfilter').slick('slickFilter', '.popular__card--tyres');
-  //   }
-  //   else if (filter == '.popular__card--electronics') {
-  //     console.log(4);
-  //     $(".popular__cardList").slick('slickUnfilter').slick('slickFilter', '.popular__card--electronics');
-  //   }
-  //   else if (filter == '.popular__card--tools') {
-  //     console.log(5);
-  //     $(".popular__cardList").slick('slickUnfilter').slick('slickFilter', '.popular__card--tools');
-  //   }
-  //   else if (filter == '.popular__card--accessory') {
-  //     console.log(6);
-  //     $(".popular__cardList").slick('slickUnfilter').slick('slickFilter', '.popular__card--accessory');
-  //   }
-
-  // })
-
-
+$(function () {
+    $('.card-like').on('click', function (e) {
+    e.preventDefault()
+    $(this).toggleClass('card-like--active');
+  })
+})
 
 
