b858cbb67a41b809a777da8b646b86997a8cc454
Sidor007 <Sidor007@yandex.ru>
2023-03-06 14:48:11 +0700

    startAdaptation4

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,6 @@ $(function () {
     arrows: false,
     appendDots: $('.sliderBanner__dots'),
     dots: true,
-    // variableWidth: true,
-    //  centerMode: true,
     slidesToShow: 1,
 
   })
@@ -56,28 +54,34 @@ $(function () {
     arrows: false,
     slidesToShow: 4,
     infiniti: true,
-    draggable: false,
+
     waitForAnimate: false,
     responsive: [
       {
-        breakpoint: 1100,
+        breakpoint: 1120,
         settings: {
           slidesToShow: 3,
-        }
+          dots: true,
+          appendDots: $('.popular__dots'),
+        },
+
+
       },
       {
         breakpoint: 950,
         settings: {
           slidesToShow: 2,
+          dots: true,
+          appendDots: $('.popular__dots'),
         }
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
-          draggable: true,
           dots: true,
           appendDots: $('.popular__dots'),
+
         }
       },
 
@@ -98,29 +102,32 @@ $(function () {
     arrows: false,
     slidesToShow: 4,
     infiniti: true,
-    draggable: false,
+
     waitForAnimate: false,
     responsive: [
       {
-        breakpoint: 1100,
+        breakpoint: 1120,
         settings: {
           slidesToShow: 3,
-        }
+          dots: true,
+          appendDots: $('.additionalItem__dots'),
+        },
+
       },
       {
         breakpoint: 950,
         settings: {
           slidesToShow: 2,
+          dots: true,
+          appendDots: $('.additionalItem__dots'),
         }
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
-          draggable: true,
           dots: true,
           appendDots: $('.additionalItem__dots'),
-
         }
       },
 
@@ -169,24 +176,24 @@ $(function () {
 // https://github.com/zgreen/ul-select
 //
 
-$.fn.ulSelect = function(){
+$.fn.ulSelect = function () {
   var ul = $(this);
 
   if (!ul.hasClass('zg-ul-select')) {
     ul.addClass('zg-ul-select');
   }
-  // SVG arrow
-  var arrow = '<svg id="ul-arrow" xmlns="http://www.w3.org/2000/svg" version="1.1" width="32" height="32" viewBox="0 0 32 32"><line stroke-width="1" x1="" y1="" x2="" y2="" stroke="#449FDB" opacity=""/><path d="M4.131 8.962c-0.434-0.429-1.134-0.429-1.566 0-0.432 0.427-0.432 1.122 0 1.55l12.653 12.528c0.434 0.429 1.133 0.429 1.566 0l12.653-12.528c0.432-0.429 0.434-1.122 0-1.55s-1.136-0.429-1.566-0.002l-11.87 11.426-11.869-11.424z" fill="#111"/></svg>';
-  $('li:first-of-type', this).addClass('active').append(arrow);
-  $(this).on('click', 'li', function(event){
-    
+
+
+  $('li:first-of-type', this).addClass('active');
+  $(this).on('click', 'li', function (event) {
+
     // Remove div#selected if it exists
     if ($('#selected--zg-ul-select').length) {
       $('#selected--zg-ul-select').remove();
     }
-    ul.before('<div id="selected--zg-ul-select">');
+    // ul.before('<div id="selected--zg-ul-select">');
     var selected = $('#selected--zg-ul-select');
-    $('li #ul-arrow', ul).remove();
+
     ul.toggleClass('active');
     // Remove active class from any <li> that has it...
     ul.children().removeClass('active');
@@ -195,55 +202,31 @@ $.fn.ulSelect = function(){
 
     var selectedText = $(this).text();
     if (ul.hasClass('active')) {
-      selected.text(selectedText).addClass('active').append(arrow);
+      selected.text(selectedText).addClass('active');
     }
     else {
-      selected.text('').removeClass('active'); 
-      $('li.active', ul).append(arrow);
+      selected.text('').removeClass('active');
+      $('li.active', ul);
     }
-    });
-    // Make ul tabbable
-    /*$(ul).focus(function(){
-     $(this).keydown(function(e) {
-      if(e.which == 38 || 40) { // Up or down keypress
-        $(this).addClass('active');
-        var liActive =  $('li.active', ul);
-        var liPrev = $('li.active', ul).prev();
-        var liNext =  $('li.active', ul).next();
-        if(e.which == 38) { // Down
-          liActive.removeClass('active');
-          liPrev.addClass('active');
-        }
-        if(e.which == 40) { // Down
-          liActive.removeClass('active');
-          liNext.addClass('active');
-        }
+  });
+
+  $(document).on('click', function (event) {
+    if ($('ul.zg-ul-select').length) {
+      if (!$('ul.zg-ul-select').has(event.target).length == 0) {
+        return;
       }
-     });
-     $(this).keydown(function(){
-        if(e.which == 13) { // Down
-          ul.removeClass('active');
-        }
-      });
-    });*/
-    // Close the faux select menu when clicking outside it 
-    $(document).on('click', function(event){
-    if($('ul.zg-ul-select').length) {
-     if(!$('ul.zg-ul-select').has(event.target).length == 0) {
-      return;
+      else {
+        $('ul.zg-ul-select').removeClass('active');
+        $('#selected--zg-ul-select').removeClass('active').text('');
+
+      }
     }
-    else {
-      $('ul.zg-ul-select').removeClass('active');
-      $('#selected--zg-ul-select').removeClass('active').text('');
-      $('#ul-arrow').remove();
-      $('ul.zg-ul-select li.active').append(arrow);
-    } 
-    }
-    });
-  }
+  });
+}
 
 // Run
-$('#be-select').ulSelect();
+$('.popular__filter-box').ulSelect();
+$('.additionalItem__filter-box').ulSelect();
 
 
 
